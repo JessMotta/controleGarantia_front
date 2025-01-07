466f80b34b39b87d73f1a30915d9c8e476d36482
Jéssica Motta <jessicalimamotta@gmail.com>
2025-01-07 16:52:00 -0300

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import './App.css';
 import Header from './components/Header';
+import styled from 'styled-components';
 
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+.button{
+  margin: 10px;
+  padding: 10px 20px;
+  border-radius:5px;
+  font-size: 14px;
+  cursor: pointer;
+  border: none;
+  background-color: #1e2229;
+  color: white;
+}`
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainer>
       <Header/>
       <div>
         <button className='button'>Cadastrar equipamento</button>
       </div>
-    </div>
+      </AppContainer>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,33 @@
-import './style.css';
 import Logo from '../../components/Logo'
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+    background-color: #282c34;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+  
+    h1 {
+      color: white;
+    }
+.button-login{
+    padding: 10px 20px;
+    border-radius:5px;
+    font-size: 14px;
+    cursor: pointer;
+    border: none;
+    background-color: white;
+    color: #1e2229;
+  }
+  `
 function Header(){
     return (
-        <header className='App-header'>
+        <HeaderContainer>
         <Logo/>
         <h1>Controle de Garantia</h1>
         <button className='button-login'>Login</button>
-    </header> 
+    </HeaderContainer> 
     )
 }
 
