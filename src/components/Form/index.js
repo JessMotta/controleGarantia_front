7f6f54d0925c9244ac7975ae118de32cd852c643
Jéssica Motta <jessicalimamotta@gmail.com>
2025-01-09 14:46:11 -0300

    added placeholder and info for form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -35,6 +35,12 @@ const ButtonsContainer = styled.div`
     margin-bottom: 20px;
 `
 
+const InfoContainer = styled.h3`
+    text-align: center;
+    font-size: 14px;
+    color: red;
+    margin-bottom: 20px;
+`
 function Form(){
     const {register, handleSubmit} = useForm();
 
@@ -47,44 +53,46 @@ function Form(){
 
 
     return (
+        <>
+        <InfoContainer>Importante: para os campos que não houverem informações coloque "Não se aplica", não deixe nenhum campo vazio</InfoContainer>
         <FormContainer onSubmit={handleSubmit(onSubmit)}>
             <LabelInputContainer>
             <LabelContainer htmlFor="Equipamento">Material/Equipamento</LabelContainer>
-            <InputContainer {...register('Equipamento', {required: true})} type="text" />
+            <InputContainer {...register('Equipamento', {required: true})} type="text" placeholder='Digite aqui o material/equipamento...' />
 
             <LabelContainer htmlFor="Local_Retirado">Retirado de</LabelContainer>
-            <InputContainer {...register('Local_Retirado')} type="text" />
+            <InputContainer {...register('Local_Retirado')} type="text" placeholder='Digite aqui o local de onde foi tirado...'/>
 
             <LabelContainer htmlFor="Coordenador_UFV">Coordenador da UFV</LabelContainer>
-            <InputContainer {...register('Coordenador_UFV')} type="text" />
+            <InputContainer {...register('Coordenador_UFV')} type="text"  placeholder='Digite aqui quem é coordenar da UFV...'/>
 
             <LabelContainer htmlFor="Pessoa_SetorResponsavel">Pessoa/Setor Responsável pela retirada</LabelContainer>
-            <InputContainer {...register('Pessoa_SetorResponsavel')} type="text" />    
+            <InputContainer {...register('Pessoa_SetorResponsavel')} type="text" placeholder='Digite aqui quem é a pessoa/setor responsável...'/>    
 
             <LabelContainer htmlFor="Data_Retirada">Data da retirada</LabelContainer>
-            <InputContainer {...register('Data_Retirada')} type="text" />   
+            <InputContainer {...register('Data_Retirada')} type="text" placeholder='Digite aqui a data da retira, formato: DD/MM/AAAA' />   
 
             <LabelContainer htmlFor="Local_Enviado">Enviado para</LabelContainer>
-            <InputContainer {...register('Local_Enviado')} type="text" />    
+            <InputContainer {...register('Local_Enviado')} type="text" placeholder='Digite aqui o local para onde foi enviado...'/>    
 
             <LabelContainer htmlFor="Local_Devolucao">Devolução para</LabelContainer>
-            <InputContainer {...register('Local_Devolucao')} type="text" />    
+            <InputContainer {...register('Local_Devolucao')} type="text" placeholder='Digite aqui o local da devolução...'/>    
 
             <LabelContainer htmlFor="Destinatario">Destinatário</LabelContainer>
-            <InputContainer {...register('Destinatario')} type="text" />
+            <InputContainer {...register('Destinatario')} type="text" placeholder='Digite aqui o destinário...'/>
 
             <LabelContainer htmlFor="Data_Devolucao">Devolução enviada em</LabelContainer>
-            <InputContainer {...register('Data_Devolucao')} type="text" />   
+            <InputContainer {...register('Data_Devolucao')} type="text" placeholder='Digite aqui a data da devolução, formato: DD/MM/AAAA' />   
 
             <LabelContainer htmlFor="Data_Instalacao">Instalado em</LabelContainer>
-            <InputContainer {...register('Data_Instalacao')} type="text" />   
+            <InputContainer {...register('Data_Instalacao')} type="text" placeholder='Digite aqui a data da instalação, formato: DD/MM/AAAA'/>   
 
             <LabelContainer htmlFor="Observacao">Observação</LabelContainer>
-            <InputContainer {...register('Observacao')} type="text" />   
+            <InputContainer {...register('Observacao')} type="text" placeholder='Digite aqui a observação...'/>   
 
             <LabelContainer htmlFor="Pessoa_Atualizacao">Atualizado por
             </LabelContainer>
-            <InputContainer {...register('Pessoa_Atualizacao')} type="text" />        
+            <InputContainer {...register('Pessoa_Atualizacao')} type="text" placeholder='Digite aqui seu nome e último sobrenome... '/>        
                        
         </LabelInputContainer>
         
@@ -93,6 +101,7 @@ function Form(){
                 <Button onClick={() => window.location.href= '/'}>Cancelar</Button>
             </ButtonsContainer>
         </FormContainer>
+        </>
     )
 }
 
